b23ac341a3f7f2be6a97b6f5df8beb19416a4831
YI ZHANG <scaccterran@gmail.com>
2018-03-06 02:27:40 -0500

    add all bonds

---
+++ div2/1000/MagicMoleculeEasy/MagicMoleculeEasy/MagicMoleculeEasy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicMoleculeEasy {
+    public class MagicMoleculeEasy {
+
+        private void AddBond(int index1, int index2, HashSet<string> bonds) {
+
+            string key1 = index1 + "," + index2;
+            string key2 = index2 + "," + index1;
+
+            if(!bonds.Contains(key1) && !bonds.Contains(key2)) {
+
+                bonds.Add(key1);
+            }
+        }
+
+        private HashSet<string> GetBonds(string[] magicBond) {
+
+            var bonds = new HashSet<string>();
+
+            for(int i = 0; i < magicBond.Length; i++ ) {
+
+                for(int j = 0; j < magicBond[i].Length; j++) {
+
+                    if(magicBond[i][j] == 'Y') {
+
+                        AddBond(i, j, bonds);
+                    }
+                }
+            }
+
+            return bonds;
+        }
+
+        public int maxMagicPower(int[] magicPower, string[] magicBond, int k) {
+
+            var bond = GetBonds(magicBond);
+
+            return 1;
+        }
+    }
+}
--- div2/1000/MagicMoleculeEasy/MagicMoleculeEasy/Program.cs
+++ div2/1000/MagicMoleculeEasy/MagicMoleculeEasy/Program.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 namespace MagicMoleculeEasy {
     class Program {
         static void Main(string[] args) {
+
+            var molecule = new MagicMoleculeEasy();
+
+            Console.WriteLine(molecule.maxMagicPower(
+
+                new int[] { 1, 2 },
+                new string[] { "NY", "YN" },
+                1
+            ));
         }
     }
 }
