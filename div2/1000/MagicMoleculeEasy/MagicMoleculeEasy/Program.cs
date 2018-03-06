3c1b9cca9aefe46fdd391973687c8641a5f4cde8
YI ZHANG <scaccterran@gmail.com>
2018-03-06 02:04:19 -0500

    SRM 571 div2 1000

---
+++ div2/1000/MagicMoleculeEasy/MagicMoleculeEasy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicMoleculeEasy {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
