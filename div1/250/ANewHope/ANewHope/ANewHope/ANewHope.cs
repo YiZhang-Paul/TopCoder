176b5bfcf7756f520c39f2a8e8c6663d45e47491
YI ZHANG <scaccterran@gmail.com>
2018-03-07 23:59:41 -0500

    count weeks

--- div1/250/ANewHope/ANewHope/ANewHope/ANewHope.cs
+++ div1/250/ANewHope/ANewHope/ANewHope/ANewHope.cs
@@ -7,9 +7,40 @@ using System.Threading.Tasks;
 namespace ANewHope {
     public class ANewHope {
 
+        private int[] ToNextWeek(int[] week, int daysToDry) {
+
+            return week.Skip(week.Length - daysToDry)
+                       .Concat(week.Take(week.Length - daysToDry))
+                       .ToArray();
+        }
+
+        private bool IsValid(int[] current, int[] target, int daysToDry) {
+
+            for(int i = 0; i < current.Length; i++) {
+
+                int days = Array.IndexOf(target, current[i]) + current.Length - i;
+
+                if(days < daysToDry) {
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         public int count(int[] firstWeek, int[] lastWeek, int daysToDry) {
 
-            return -1;
+            int weeks = 1;
+            int[] current = firstWeek;
+
+            while(!IsValid(current, lastWeek, daysToDry)) {
+
+                current = ToNextWeek(current, daysToDry);
+                weeks++;
+            }
+
+            return weeks == 1 ? weeks : weeks + 1;
         }
     }
 }
