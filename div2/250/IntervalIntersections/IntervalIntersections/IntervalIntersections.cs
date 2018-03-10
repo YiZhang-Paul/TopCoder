3d89a8c33d3a8b0e892a64bad5294274c8557016
YI ZHANG <scaccterran@gmail.com>
2018-03-10 06:51:40 -0500

    SRM 730 div2 250

---
+++ div2/250/IntervalIntersections/IntervalIntersections/IntervalIntersections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntervalIntersections {
+    public class IntervalIntersections {
+
+        public int minLength(int x1, int y1, int x2, int y2) {
+
+            return -1;
+        }
+    }
+}
---
+++ div2/250/IntervalIntersections/Test/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntervalIntersections;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        IntervalIntersections.IntervalIntersections intersection;
+
+        [TestInitialize]
+        public void Setup() {
+
+            intersection = new IntervalIntersections.IntervalIntersections();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            Assert.AreEqual(1, intersection.minLength(3, 6, 1, 2));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            Assert.AreEqual(1, intersection.minLength(1, 2, 3, 6));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            Assert.AreEqual(0, intersection.minLength(1, 10, 2, 5));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            Assert.AreEqual(0, intersection.minLength(4, 5, 1, 4));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            Assert.AreEqual(999999, intersection.minLength(1, 1, 1000000, 1000000));
+        }
+    }
+}
