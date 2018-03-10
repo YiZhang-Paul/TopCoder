14a28fffc7750da90c886ad75c4fc6cd27261a2d
YI ZHANG <scaccterran@gmail.com>
2018-03-10 07:10:28 -0500

    all done

--- div2/250/IntervalIntersections/IntervalIntersections/IntervalIntersections.cs
+++ div2/250/IntervalIntersections/IntervalIntersections/IntervalIntersections.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 namespace IntervalIntersections {
     public class IntervalIntersections {
 
+        private bool IsIntersect(int x1, int y1, int x2, int y2) {
+
+            return (x2 >= x1 && x2 <= y1) || (x1 >= x2 && x1 <= y2);
+        }
+
         public int minLength(int x1, int y1, int x2, int y2) {
 
-            return -1;
+            if(IsIntersect(x1, y1, x2, y2)) {
+
+                return 0;
+            }
+            //return distance between two intervals
+            return x2 > y1 ? x2 - y1 : x1 - y2;
         }
     }
 }
