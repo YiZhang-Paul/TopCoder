f19ff37b4ef9331dd2a10098cbaf6f52d0aa27d3
YI ZHANG <scaccterran@gmail.com>
2018-03-10 06:34:29 -0500

    all done

---
+++ div2/250/StringHalving/StringHalving/StringHalving.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringHalving {
+    public class StringHalving {
+
+        private string GetUniqueLetters(string text) {
+
+            var uniqueLetters = new HashSet<char>(text.ToArray());
+
+            return string.Join("", uniqueLetters.ToArray());
+        }
+
+        private string SortLetters(string text) {
+            //sort letters according to alphabetical order
+            return string.Join("", text.OrderBy(letter => letter));
+        }
+
+        //retrieve the first N letters in a given string
+        private string Slice(string text, int index) {
+
+            return string.Join("", text.Take(index));
+        }
+
+        private bool HasDuplicate(string text) {
+            //check if all letters in the text are unique
+            return GetUniqueLetters(text).Count() != text.Length;
+        }
+
+        public string lexsmallest(string text) {
+            //start with smallest letter possible
+            foreach(char letter in SortLetters(GetUniqueLetters(text))) {
+
+                string prefix = Slice(text, text.IndexOf(letter));
+                //make sure every other letters before current letter can be removed
+                if(!HasDuplicate(prefix)) {
+
+                    return letter.ToString();
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ div2/250/StringHalving/Test/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringHalving;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        StringHalving.StringHalving stringHalf;
+
+        [TestInitialize]
+        public void Setup() {
+
+            stringHalf = new StringHalving.StringHalving();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            Assert.AreEqual("a", stringHalf.lexsmallest("baba"));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            Assert.AreEqual("b", stringHalf.lexsmallest("bbaa"));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            Assert.AreEqual("g", stringHalf.lexsmallest("zyiggiyssz"));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            Assert.AreEqual("c", stringHalf.lexsmallest("topcodertpcder"));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            Assert.AreEqual("f", stringHalf.lexsmallest("rvofqorvfq"));
+        }
+    }
+}
