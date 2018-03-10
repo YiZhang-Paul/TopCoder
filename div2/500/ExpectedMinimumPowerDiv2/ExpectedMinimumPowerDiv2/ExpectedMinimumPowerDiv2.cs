022330b261c8193f2931eccccc5ca74a57a2fa6b
YI ZHANG <scaccterran@gmail.com>
2018-03-10 07:24:19 -0500

    SRM 730 div2 500

---
+++ div2/500/ExpectedMinimumPowerDiv2/ExpectedMinimumPowerDiv2/ExpectedMinimumPowerDiv2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpectedMinimumPowerDiv2 {
+    public class ExpectedMinimumPowerDiv2 {
+
+        public double findExp(int n, int x) {
+
+            return 1.0;
+        }
+    }
+}
---
+++ div2/500/ExpectedMinimumPowerDiv2/Test/Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpectedMinimumPowerDiv2;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        ExpectedMinimumPowerDiv2.ExpectedMinimumPowerDiv2 minPower;
+
+        [TestInitialize]
+        public void Setup() {
+
+            minPower = new ExpectedMinimumPowerDiv2.ExpectedMinimumPowerDiv2();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            Assert.AreEqual(2.0, minPower.findExp(4, 4));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            Assert.AreEqual(2.6666666666666665, minPower.findExp(3, 2));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            Assert.AreEqual(4.666666666666667, minPower.findExp(3, 1));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            Assert.AreEqual(8.076190476190476, minPower.findExp(10, 4));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            Assert.AreEqual(9.906688596554163, minPower.findExp(50, 25));
+        }
+
+        [TestMethod]
+        public void Sample6() {
+
+            Assert.AreEqual(4.503599627370492E13, minPower.findExp(50, 1));
+        }
+    }
+}
