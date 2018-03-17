35003f524425c0ef9f03730c3bc3104a9ddad338
YI ZHANG <scaccterran@gmail.com>
2018-03-17 10:48:13 -0400

    all done

--- div2/500/SoManyRectangles/SoManyRectangles/SoManyRectangles.cs
+++ div2/500/SoManyRectangles/SoManyRectangles/SoManyRectangles.cs
@@ -13,20 +13,6 @@ namespace SoManyRectangles {
         public int X2 { get; private set; }
         public int Y2 { get; private set; }
 
-        public List<int[]> Corners {
-
-            get {
-
-                return new List<int[]>() {
-
-                    new int[] { X1, Y1 },
-                    new int[] { X1, Y2 },
-                    new int[] { X2, Y2 },
-                    new int[] { X2, Y1 }
-                };
-            }
-        }
-
         public CustomRectangle(int x1, int y1, int x2, int y2) {
 
             X1 = x1;
@@ -35,60 +21,44 @@ namespace SoManyRectangles {
             Y2 = y2;
         }
 
-        private bool IsSame(CustomRectangle rectangle) {
+        public static bool IsOverlap(CustomRectangle first, CustomRectangle second) {
+            //check if either rectangle's left edge is greater or equal to other rectangle's right edge
+            if(first.X1 >= second.X2 || second.X1 >= first.X2) {
 
-            return X1 == rectangle.X1 && X2 == rectangle.X2 && Y1 == rectangle.Y1 && Y2 == rectangle.Y2;
-        }
+                return false;
+            }
+            //check if either rectangle's bottom edge is greater or equal to other rectangle's top edge
+            if(first.Y1 >= second.Y2 || second.Y1 >= first.Y2) {
 
-        private bool HasPoint(int x, int y, CustomRectangle rectangle) {
-
-            return rectangle.X1 < x && x < rectangle.X2 && rectangle.Y1 < y && y < rectangle.Y2;
-        }
-
-        public bool IsOverlap(CustomRectangle rectangle) {
-
-            if(IsSame(rectangle)) {
-
-                return true;
+                return false;
             }
 
-            return Corners.Any(corner => HasPoint(corner[0], corner[1], rectangle)) ||
-                   rectangle.Corners.Any(corner => HasPoint(corner[0], corner[1], this));
+            return true;
         }
     }
 
     public class SoManyRectangles {
 
-        private List<CustomRectangle> GetRectangles(int[] x1, int[] y1, int[] x2, int[] y2) {
+        public int maxOverlap(int[] x1, int[] y1, int[] x2, int[] y2) {
 
-            var rectangles = new List<CustomRectangle>();
+            int max = 0;
 
             for(int i = 0; i < x1.Length; i++) {
 
-                rectangles.Add(new CustomRectangle(x1[i], y1[i], x2[i], y2[i]));
-            }
+                int overlap = 0;
+                var current = new CustomRectangle(x1[i], y1[i], x2[i], y2[i]);
 
-            return rectangles;
-        }
+                for(int j = 0; j < x1.Length; j++) {
 
-        public int maxOverlap(int[] x1, int[] y1, int[] x2, int[] y2) {
+                    var other = new CustomRectangle(x1[j], y1[j], x2[j], y2[j]);
 
-            var rectangles = GetRectangles(x1, y1, x2, y2);
-            int max = 0;
+                    if(CustomRectangle.IsOverlap(current, other)) {
 
-            foreach(var rectangle in rectangles) {
-
-                int current = 1;
-
-                foreach(var other in rectangles) {
-
-                    if(other != rectangle && rectangle.IsOverlap(other)) {
-
-                        current++;
+                        overlap++;
                     }
                 }
 
-                max = Math.Max(max, current);
+                max = Math.Max(max, overlap);
             }
 
             return max;
