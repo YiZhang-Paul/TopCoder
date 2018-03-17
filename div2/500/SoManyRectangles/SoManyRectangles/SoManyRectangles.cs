9a7bd1c1199cad871d7bfd4303afce8481cc0ec8
YI ZHANG <scaccterran@gmail.com>
2018-03-17 08:52:49 -0400

    SRM 729 div2 500

---
+++ div2/500/SoManyRectangles/SoManyRectangles/SoManyRectangles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoManyRectangles {
+    public class SoManyRectangles {
+
+        public int maxOverlap(int[] x1, int[] y1, int[] x2, int[] y2) {
+
+            return -1;
+        }
+    }
+}
---
+++ div2/500/SoManyRectangles/Test/Test.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoManyRectangles;
+
+namespace Test {
+
+    [TestClass]
+    public class Test {
+
+        SoManyRectangles.SoManyRectangles rectangles;
+
+        [TestInitialize]
+        public void Setup() {
+
+            rectangles = new SoManyRectangles.SoManyRectangles();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            int[] x1 = { 0, 50 };
+            int[] y1 = { 0, 50 };
+            int[] x2 = { 100, 60 };
+            int[] y2 = { 100, 60 };
+
+            Assert.AreEqual(2, rectangles.maxOverlap(x1, y1, x2, y2));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            int[] x1 = { 0, 90 };
+            int[] y1 = { 0, 90 };
+            int[] x2 = { 100, 200 };
+            int[] y2 = { 100, 200 };
+
+            Assert.AreEqual(2, rectangles.maxOverlap(x1, y1, x2, y2));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            int[] x1 = { 0, 100 };
+            int[] y1 = { 0, 100 };
+            int[] x2 = { 100, 200 };
+            int[] y2 = { 100, 200 };
+
+            Assert.AreEqual(1, rectangles.maxOverlap(x1, y1, x2, y2));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            int[] x1 = { 0, 0, 0, 0, 0 };
+            int[] y1 = { 0, 0, 0, 0, 0 };
+            int[] x2 = { 1, 1, 1, 1, 1 };
+            int[] y2 = { 1, 1, 1, 1, 1 };
+
+            Assert.AreEqual(5, rectangles.maxOverlap(x1, y1, x2, y2));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            int[] x1 = { 0 };
+            int[] y1 = { 0 };
+            int[] x2 = { 1 };
+            int[] y2 = { 1 };
+
+            Assert.AreEqual(1, rectangles.maxOverlap(x1, y1, x2, y2));
+        }
+    }
+}
