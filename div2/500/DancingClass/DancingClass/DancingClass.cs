2e43eaf915ca7bcb127a7b90e75bc979943cafad
YI ZHANG <scaccterran@gmail.com>
2018-03-18 10:07:40 -0400

    all done

--- div2/500/DancingClass/DancingClass/DancingClass.cs
+++ div2/500/DancingClass/DancingClass/DancingClass.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace DancingClass {
     public class DancingClass {
+        //count total combination of picking subsets with given size
+        private double TotalCombination(int total, int size) {
+
+            double combination = 1;
+
+            for(int i = 0; i < size; i++) {
+
+                combination *= total - i;
+                combination /= size - i;
+            }
+
+            return combination;
+        }
+
+        private string PrintOdds(double odds) {
+
+            if(odds == 0.5) {
+
+                return "Equal";
+            }
+
+            return odds < 0.5 ? "Low" : "High";
+        }
 
         public string checkOdds(int total, int pairs) {
 
-            return "";
+            double satisfied = 0;
+            //total number of boys in different cases
+            for(int i = 0; i <= total; i++) {
+                //total number of distinct pairs
+                if(i * (total - i) >= pairs) {
+
+                    satisfied += TotalCombination(total, i);
+                }
+            }
+            //check possibility
+            for(int i = 0; i < total; i++) {
+
+                satisfied /= 2;
+            }
+
+            return PrintOdds(satisfied);
         }
     }
 }
