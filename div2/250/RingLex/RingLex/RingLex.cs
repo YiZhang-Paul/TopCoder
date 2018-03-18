527d29214ef1f8d7f356c8e7bd0665c348ad9d33
YI ZHANG <scaccterran@gmail.com>
2018-03-18 08:07:30 -0400

    SRM 731 div2 250

---
+++ div2/250/RingLex/RingLex/RingLex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RingLex {
+    public class RingLex {
+
+        public string getmin(string text) {
+
+            return "";
+        }
+    }
+}
---
+++ div2/250/RingLex/Test/Test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RingLex;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        RingLex.RingLex ringLex;
+
+        [TestInitialize]
+        public void Setup() {
+
+            ringLex = new RingLex.RingLex();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            Assert.AreEqual("abc", ringLex.getmin("cba"));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            Assert.AreEqual("abc", ringLex.getmin("acb"));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            Assert.AreEqual("aaaabcb", ringLex.getmin("abacaba"));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            Assert.AreEqual("aabab", ringLex.getmin("aaabb"));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            Assert.AreEqual("abazabaz", ringLex.getmin("azzzabbb"));
+        }
+
+        [TestMethod]
+        public void Sample6() {
+
+            Assert.AreEqual("aaaaaa", ringLex.getmin("abbaac"));
+        }
+
+        [TestMethod]
+        public void Sample7() {
+
+            Assert.AreEqual("dehifsfsoy", ringLex.getmin("fsdifyhsoe"));
+        }
+    }
+}
