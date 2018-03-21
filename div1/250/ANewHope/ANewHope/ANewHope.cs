f8477ffb5f811bd8ef3a6500d5d1d76d43b5fce0
YI ZHANG <scaccterran@gmail.com>
2018-03-20 22:43:26 -0400

    all done

--- div1/250/ANewHope/ANewHope/ANewHope.cs
+++ div1/250/ANewHope/ANewHope/ANewHope.cs
@@ -5,55 +5,54 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ANewHope {
+
+    /**
+     * Reasoning:
+     *
+     * since days to dry a shirt is always less than N (total days in week),
+     * for any given shirt used on day i in current week, it can
+     * be used on any day between day i and day N (inclusive) during the next week;
+     * that is to say, any shirt can stay in its original position or move
+     * anywhere to its right during next week. Thus, shirts that need to
+     * stay or move to the right on final week is not the problem here - it is those
+     * shirts that need to move to the left. The earliest day a shirt can be dressed again
+     * is: (day in current week) - (N - days to dry); if it results in a negative index, it
+     * refers to the day in last week (take calendar for example). Therefore, the maximum
+     * number of days to move any shirt to left during one week is (N - days to dry).
+     * The question then comes down to finding the shirt with maximum number of days
+     * to move to left, and minimum number of weeks to do so.
+     */
+
     public class ANewHope {
 
-        //wear T-shirst as soons as they are dry
-        private void WearWhenDry(ref int[] current, int daysToDry) {
+        private Dictionary<int, int> RecordIndex(int[] array) {
 
-            int shiftPoint = current.Length - daysToDry;
-            //shift every T-shirt by total days to dry
-            current = current.Skip(shiftPoint)
-                             .Concat(current.Take(shiftPoint))
-                             .ToArray();
+            var indexes = new Dictionary<int, int>();
+
+            for(int i = 0; i < array.Length; i++) {
+
+                indexes[array[i]] = i;
+            }
+
+            return indexes;
         }
 
-        private int DaysBetween(int start, int end, int daysInWeek) {
+        public int count(int[] first, int[] last, int daysToDry) {
 
-            return (daysInWeek - start - 1) + (end + 1);
-        }
+            var indexes = RecordIndex(first);
+            int maxDaysToLeft = 0;
+            //find maximum number of days for any shirt that needs to move to the left
+            for(int i = 0; i < last.Length; i++) {
 
-        //check if T-shirts can dry before corresponding days in target week
-        private bool CanDryBefore(int[] current, int[] target, int daysToDry) {
+                int initialIndex = indexes[last[i]];
 
-            for(int i = 0; i < current.Length; i++) {
+                if(i < initialIndex) {
 
-                int targetDay = Array.IndexOf(target, current[i]);
-
-                if(DaysBetween(i, targetDay, current.Length) < daysToDry) {
-
-                    return false;
+                    maxDaysToLeft = Math.Max(maxDaysToLeft, initialIndex - i);
                 }
             }
-
-            return true;
-        }
-
-        public int count(int[] firstWeek, int[] lastWeek, int daysToDry) {
-
-            if(CanDryBefore(firstWeek, lastWeek, daysToDry)) {
-
-                return 1;
-            }
-            //include first and last week on default
-            int total = 2;
-
-            while(!CanDryBefore(firstWeek, lastWeek, daysToDry)) {
-
-                WearWhenDry(ref firstWeek, daysToDry);
-                total++;
-            }
-
-            return total;
+            //find minimum number of weeks to move the above shirt to its final position
+            return (int)Math.Ceiling((double)maxDaysToLeft / (first.Length - daysToDry)) + 1;
         }
     }
 }
--- div2/250/StringHalving/StringHalving/StringHalving.cs
+++ div2/250/StringHalving/StringHalving/StringHalving.cs
@@ -7,38 +7,22 @@ using System.Threading.Tasks;
 namespace StringHalving {
     public class StringHalving {
 
-        private string GetUniqueLetters(string text) {
+        private char GetMinCharacter(string text) {
 
-            var uniqueLetters = new HashSet<char>(text.ToArray());
-
-            return string.Join("", uniqueLetters.ToArray());
-        }
-
-        private string SortLetters(string text) {
-            //sort letters according to alphabetical order
-            return string.Join("", text.OrderBy(letter => letter));
-        }
-
-        //retrieve the first N letters in a given string
-        private string Slice(string text, int index) {
-
-            return string.Join("", text.Take(index));
-        }
-
-        private bool HasDuplicate(string text) {
-            //check if all letters in the text are unique
-            return GetUniqueLetters(text).Count() != text.Length;
+            return text.OrderBy(character => character).First();
         }
 
         public string lexsmallest(string text) {
-            //start with smallest letter possible
-            foreach(char letter in SortLetters(GetUniqueLetters(text))) {
 
-                string prefix = Slice(text, text.IndexOf(letter));
-                //make sure every other letters before current letter can be removed
-                if(!HasDuplicate(prefix)) {
+            int[] occurrence = new int[26];
 
-                    return letter.ToString();
+            for(int i = 0; i < text.Length; i++) {
+                //when duplicate letter is found
+                if(++occurrence[text[i] - 'a'] == 2) {
+                    //only letters before can remain in front after removing duplicates
+                    string prefix = text.Substring(0, i);
+
+                    return GetMinCharacter(prefix).ToString();
                 }
             }
 
