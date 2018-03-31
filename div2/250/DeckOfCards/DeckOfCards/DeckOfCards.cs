db62c05f0fdd0b7b07039efcd2dc165c53ca735c
YI ZHANG <scaccterran@gmail.com>
2018-03-31 03:01:31 -0400

    test card deck

--- div2/250/DeckOfCards/DeckOfCards/DeckOfCards.cs
+++ div2/250/DeckOfCards/DeckOfCards/DeckOfCards.cs
@@ -7,9 +7,54 @@ using System.Threading.Tasks;
 namespace DeckOfCards {
     public class DeckOfCards {
 
+        private Dictionary<int, HashSet<char>> GetCards(int[] value, string suit) {
+
+            var cards = new Dictionary<int, HashSet<char>>();
+
+            for(int i = 0; i < value.Length; i++) {
+
+                if(!cards.ContainsKey(value[i])) {
+
+                    cards[value[i]] = new HashSet<char>();
+                }
+
+                cards[value[i]].Add(suit[i]);
+            }
+
+            return cards;
+        }
+
+        private int CountCards(Dictionary<int, HashSet<char>> cards) {
+
+            return cards.Sum(pair => pair.Value.Count);
+        }
+
         public string IsValid(int n, int[] value, string suit) {
 
-            return "";
+            var cards = GetCards(value, suit);
+
+            if(CountCards(cards) != n) {
+
+                return "Not perfect";
+            }
+
+            for(int i = 0; i < value.Length - 1; i++) {
+
+                for(int j = i + 1; j < value.Length; j++) {
+
+                    if(!cards[value[i]].Contains(suit[j])) {
+
+                        return "Not perfect";
+                    }
+
+                    if(!cards[value[j]].Contains(suit[i])) {
+
+                        return "Not perfect";
+                    }
+                }
+            }
+
+            return "Perfect";
         }
     }
 }
