e18801691961813ccd7e88ec959ab23ed89fd927
YI ZHANG <scaccterran@gmail.com>
2018-04-01 20:32:28 -0400

    SRM 732 div1 250

---
+++ div1/250/TileFlippingGame/Test/Test.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TileFlippingGame;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        TileFlippingGame.TileFlippingGame flip;
+
+        [TestInitialize]
+        public void Setup() {
+
+            flip = new TileFlippingGame.TileFlippingGame();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            string[] board = { "bbb", "eee", "www" };
+
+            Assert.AreEqual(1, flip.HowManyMoves(3, 3, board));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            string[] board = { "bwe", "wbw", "ewb" };
+
+            Assert.AreEqual(2, flip.HowManyMoves(3, 3, board));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            string[] board = { "beww", "beww", "beww", "wewe" };
+
+            Assert.AreEqual(1, flip.HowManyMoves(4, 4, board));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            string[] board = { "ewewbbbb", "bwebewww" };
+
+            Assert.AreEqual(3, flip.HowManyMoves(2, 8, board));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            string[] board = { "bwebw", "wbebb", "eeeee", "bbebb", "bbebb" };
+
+            Assert.AreEqual(3, flip.HowManyMoves(5, 5, board));
+        }
+
+        [TestMethod]
+        public void Sample6() {
+
+            string[] board = { "bwbbbbb", "bwbwwwb", "bwbwewb", "bwbbbwb", "bwwwwwb", "bbbbbbb" };
+
+            Assert.AreEqual(1, flip.HowManyMoves(6, 7, board));
+        }
+
+        [TestMethod]
+        public void Sample7() {
+
+            string[] board = { "bwbbbbb", "bwbwwwb", "eeeeeee", "bwbbbwb", "bwwwwwb", "bbbbbbb" };
+
+            Assert.AreEqual(3, flip.HowManyMoves(6, 7, board));
+        }
+    }
+}
---
+++ div1/250/TileFlippingGame/TileFlippingGame/TileFlippingGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileFlippingGame {
+    public class TileFlippingGame {
+
+        public int HowManyMoves(int n, int m, string[] X) {
+
+            return -1;
+        }
+    }
+}
