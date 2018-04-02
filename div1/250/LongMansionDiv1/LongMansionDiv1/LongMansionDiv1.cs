3ba37f5d321772355d3f087d8ab7280613769d37
YI ZHANG <scaccterran@gmail.com>
2018-04-02 07:05:52 -0400

    find shortest path

--- div1/250/LongMansionDiv1/LongMansionDiv1/LongMansionDiv1.cs
+++ div1/250/LongMansionDiv1/LongMansionDiv1/LongMansionDiv1.cs
@@ -7,9 +7,38 @@ using System.Threading.Tasks;
 namespace LongMansionDiv1 {
     public class LongMansionDiv1 {
 
+        private long GetHorizontalTime(int[] t, int column1, int column2, int row) {
+
+            return (long)(Math.Abs(column2 - column1) + 1) * t[row];
+        }
+
+        private long GetVerticalTime(int[] t, int row1, int row2) {
+
+            int start = Math.Min(row1, row2);
+            int end = Math.Max(row1, row2);
+            long time = 0;
+
+            for(int i = start + 1; i <= end; i++) {
+
+                time += t[i];
+            }
+
+            return time;
+        }
+
         public long minimalTime(int[] t, int sX, int sY, int eX, int eY) {
 
-            return -1;
+            long minTime = 0;
+            int maxUp = 0 - sX;
+            int maxDown = t.Length - 1 - sX;
+
+            for(int i = maxUp; i <= maxDown; i++) {
+
+                long time = GetVerticalTime(t, sX, sX + i) + GetHorizontalTime(t, sY, eY, sX + i) + GetVerticalTime(t, sX + i, eX);
+                minTime = minTime == 0 ? time : Math.Min(minTime, time);
+            }
+
+            return minTime;
         }
     }
 }
