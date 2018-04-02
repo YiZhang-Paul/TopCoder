46e67ce158f49c7025f26cd78551b834a8203eff
YI ZHANG <scaccterran@gmail.com>
2018-04-02 06:02:36 -0400

    all done

--- div1/250/TileFlippingGame/TileFlippingGame/TileFlippingGame.cs
+++ div1/250/TileFlippingGame/TileFlippingGame/TileFlippingGame.cs
@@ -8,20 +8,24 @@ namespace TileFlippingGame {
     public class TileFlippingGame {
 
         private Dictionary<int, HashSet<int>> Visited { get; set; }
-        private char[][] Board { get; set; }
 
-        private void GetBoard(string[] x) {
+        private char[][] GetBoard(string[] x) {
 
-            Board = x.Select(row => row.ToCharArray()).ToArray();
+            return x.Select(row => {
+
+                return row.ToCharArray();
+
+            }).ToArray();
         }
 
-        private List<int[]> GetAllGrids() {
+        //retrieve all grids on a board
+        private List<int[]> GetGrids(char[][] board) {
 
             var grids = new List<int[]>();
 
-            for(int i = 0; i < Board.Length; i++) {
+            for(int i = 0; i < board.Length; i++) {
 
-                for(int j = 0; j < Board[0].Length; j++) {
+                for(int j = 0; j < board[0].Length; j++) {
 
                     grids.Add(new int[] { i, j });
                 }
@@ -40,24 +44,36 @@ namespace TileFlippingGame {
             return Visited[row].Contains(column);
         }
 
-        private bool IsEmpty(int row, int column) {
+        private bool IsEmpty(char[][] board, int row, int column) {
 
-            return Board[row][column] == 'e';
+            return board[row][column] == 'e';
         }
 
-        private bool IsOutOfBound(int row, int column) {
+        //check if given grid is within board boundary
+        private bool IsOutOfBound(char[][] board, int row, int column) {
 
-            if(row < 0 || row > Board.Length - 1) {
+            if(row < 0 || row > board.Length - 1) {
 
                 return true;
             }
 
-            if(column < 0 || column > Board[0].Length - 1) {
+            if(column < 0 || column > board[0].Length - 1) {
 
                 return true;
             }
 
             return false;
+        }
+
+        //check if given grid is unvisited non empty grid
+        private bool IsValidGrid(char[][] board, int row, int column) {
+
+            if(IsOutOfBound(board, row, column)) {
+
+                return false;
+            }
+
+            return !IsVisited(row, column) && !IsEmpty(board, row, column);
         }
 
         private void AddVisited(int row, int column) {
@@ -70,48 +86,56 @@ namespace TileFlippingGame {
             Visited[row].Add(column);
         }
 
-        private List<int[]> FindBlock(char[][] board, int row, int column, List<int[]> block, bool useSeparator) {
+        /*
+         * get block of non empty grids that are adjacent to each other;
+         * only get grids of same color when separate is set to true
+         */
+        private List<int[]> GetBlock(char[][] board, int row, int column, List<int[]> block, bool separate) {
 
-            if(IsOutOfBound(row, column) || IsVisited(row, column) || IsEmpty(row, column)) {
+            if(!IsValidGrid(board, row, column)) {
 
                 return null;
             }
 
-            if(block.Count > 0 && useSeparator && board[row][column] != board[block[0][0]][block[0][1]]) {
+            if(block.Count > 0 && separate) {
 
-                return null;
+                if(board[row][column] != board[block[0][0]][block[0][1]]) {
+
+                    return null;
+                }
             }
 
             AddVisited(row, column);
             block.Add(new int[] { row, column });
 
-            FindBlock(board, row + 1, column, block, useSeparator);
-            FindBlock(board, row - 1, column, block, useSeparator);
-            FindBlock(board, row, column + 1, block, useSeparator);
-            FindBlock(board, row, column - 1, block, useSeparator);
+            GetBlock(board, row + 1, column, block, separate);
+            GetBlock(board, row - 1, column, block, separate);
+            GetBlock(board, row, column + 1, block, separate);
+            GetBlock(board, row, column - 1, block, separate);
 
             return block;
         }
 
-        private List<List<int[]>> FindBlocks(char[][] board, List<int[]> grids, bool useSeparator) {
+        private List<List<int[]>> GetBlocks(char[][] board, List<int[]> grids, bool separate) {
 
             Visited = new Dictionary<int, HashSet<int>>();
             var blocks = new List<List<int[]>>();
 
             foreach(var grid in grids) {
 
-                if(IsVisited(grid[0], grid[1]) || IsEmpty(grid[0], grid[1])) {
+                if(IsVisited(grid[0], grid[1]) || IsEmpty(board, grid[0], grid[1])) {
 
                     continue;
                 }
 
-                blocks.Add(FindBlock(board, grid[0], grid[1], new List<int[]>(), useSeparator));
+                blocks.Add(GetBlock(board, grid[0], grid[1], new List<int[]>(), separate));
             }
 
             return blocks;
         }
 
-        private void FlipBlock(char[][] board, List<int[]> block) {
+        //flip all grids in a block
+        private void Flip(char[][] board, List<int[]> block) {
 
             char tile = board[block[0][0]][block[0][1]] == 'b' ? 'w' : 'b';
 
@@ -121,6 +145,12 @@ namespace TileFlippingGame {
             }
         }
 
+        private char[][] CopyBoard(char[][] board) {
+
+            return board.Select(row => row.ToArray()).ToArray();
+        }
+
+        //count total steps needed to flip a block until all tiles have same color
         private int CountSteps(char[][] board, List<int[]> block, int total, ref char result) {
 
             int steps = 0;
@@ -128,9 +158,9 @@ namespace TileFlippingGame {
             while(block.Count != total) {
 
                 steps++;
-                FlipBlock(board, block);
+                Flip(board, block);
                 Visited.Clear();
-                block = FindBlock(board, block[0][0], block[0][1], new List<int[]>(), true);
+                block = GetBlock(board, block[0][0], block[0][1], new List<int[]>(), true);
             }
 
             result = board[block[0][0]][block[0][1]];
@@ -138,20 +168,16 @@ namespace TileFlippingGame {
             return steps;
         }
 
-        private char[][] CopyBoard() {
+        //count minimum steps needed to make all tiles in a block have same color
+        private int[] CountMinSteps(char[][] board, List<int[]> block) {
 
-            return Board.Select(row => row.ToArray()).ToArray();
-        }
+            int black = -1; //steps needed to make all tiles black
+            int white = -1; //steps needed to make all tiles white
 
-        private int[] GetMinSteps(List<int[]> block) {
+            foreach(var subBlock in GetBlocks(board, block, true)) {
 
-            int black = -1;
-            int white = -1;
-
-            foreach(var subBlock in FindBlocks(Board, block, true)) {
-
-                char result = '\0';
-                int steps = CountSteps(CopyBoard(), subBlock, block.Count, ref result);
+                char result = '\0'; //resulting color
+                int steps = CountSteps(CopyBoard(board), subBlock, block.Count, ref result);
 
                 if(result == 'b') {
 
@@ -162,7 +188,7 @@ namespace TileFlippingGame {
                     white = white == -1 ? steps : Math.Min(white, steps);
                 }
             }
-
+            //-1 means the block must be flipped one more time to get given color
             black = black == -1 ? white + 1 : black;
             white = white == -1 ? black + 1 : white;
 
@@ -171,9 +197,9 @@ namespace TileFlippingGame {
 
         public int HowManyMoves(int n, int m, string[] x) {
 
-            GetBoard(x);
-            var blocks = FindBlocks(Board, GetAllGrids(), false);
-            var steps = blocks.Select(block => GetMinSteps(block));
+            var board = GetBoard(x);
+            var blocks = GetBlocks(board, GetGrids(board), false);
+            var steps = blocks.Select(block => CountMinSteps(board, block));
 
             return Math.Min(steps.Sum(step => step[0]), steps.Sum(step => step[1]));
         }
