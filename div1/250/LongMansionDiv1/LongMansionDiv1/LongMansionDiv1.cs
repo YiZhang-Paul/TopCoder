4ed6b67b218df240548be9bec7f70e9c43e01e52
YI ZHANG <scaccterran@gmail.com>
2018-04-02 08:22:48 -0400

    all done

--- div1/250/LongMansionDiv1/LongMansionDiv1/LongMansionDiv1.cs
+++ div1/250/LongMansionDiv1/LongMansionDiv1/LongMansionDiv1.cs
@@ -7,16 +7,16 @@ using System.Threading.Tasks;
 namespace LongMansionDiv1 {
     public class LongMansionDiv1 {
 
-        private long GetHorizontalTime(int[] t, int column1, int column2, int row) {
+        private long GetHorizontalTime(int start, int end, int speed) {
 
-            return (long)(Math.Abs(column2 - column1) - 1) * t[row];
+            return (long)(Math.Abs(start - end) + 1) * speed;
         }
 
         private long GetVerticalTime(int[] t, int row1, int row2) {
 
+            long time = 0;
             int start = Math.Min(row1, row2);
             int end = Math.Max(row1, row2);
-            long time = 0;
 
             for(int i = start; i <= end; i++) {
 
@@ -26,19 +26,25 @@ namespace LongMansionDiv1 {
             return time;
         }
 
+        private long GetTime(int[] t, int sX, int sY, int eX, int eY, int row) {
+
+            long vertical = GetVerticalTime(t, sX, row) + GetVerticalTime(t, row, eX);
+            long horizontal = GetHorizontalTime(sY, eY, t[row]);
+            //remove duplicate time calculation
+            return vertical + horizontal - 2 * t[row];
+        }
+
         public long minimalTime(int[] t, int sX, int sY, int eX, int eY) {
 
-            long minTime = 0;
-            int maxUp = 0 - sX;
-            int maxDown = t.Length - 1 - sX;
+            long min = 0;
 
-            for(int i = maxUp; i <= maxDown; i++) {
+            for(int i = 0; i < t.Length; i++) {
 
-                long time = GetVerticalTime(t, sX, sX + i) + GetHorizontalTime(t, sY, eY, sX + i) + GetVerticalTime(t, sX + i, eX);
-                minTime = minTime == 0 ? time : Math.Min(minTime, time);
+                long time = GetTime(t, sX, sY, eX, eY, i);
+                min = min == 0 ? time : Math.Min(min, time);
             }
 
-            return minTime;
+            return min;
         }
     }
 }
