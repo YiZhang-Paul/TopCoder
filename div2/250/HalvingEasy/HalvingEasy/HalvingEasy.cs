f86c32ffceda510cfc5190119b6f3f47a7440602
YI ZHANG <scaccterran@gmail.com>
2018-04-04 00:34:48 -0400

    all done

--- div2/250/HalvingEasy/HalvingEasy/HalvingEasy.cs
+++ div2/250/HalvingEasy/HalvingEasy/HalvingEasy.cs
@@ -7,24 +7,20 @@ using System.Threading.Tasks;
 namespace HalvingEasy {
     public class HalvingEasy {
 
+        //check if given value can be halved to target value
         private bool CanDivide(int value, int target) {
 
-            if(value <= target) {
+            while(value >= target) {
 
-                return value == target;
-            }
+                if(value == target) {
 
-            while(value > target) {
+                    return true;
+                }
 
                 value /= 2;
-
-                if(value < target) {
-
-                    return false;
-                }
             }
 
-            return true;
+            return false;
         }
 
         public int count(int[] s, int t) {
