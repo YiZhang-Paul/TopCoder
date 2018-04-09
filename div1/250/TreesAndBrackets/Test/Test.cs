b65463e1f464607cdebc8122f732979bfed6bc75
YI ZHANG <scaccterran@gmail.com>
2018-04-09 17:15:34 -0400

    SRM 731 div1 250

---
+++ div1/250/TreesAndBrackets/Test/Test.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreesAndBrackets;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        TreesAndBrackets.TreesAndBrackets brackets;
+
+        [TestInitialize]
+        public void Setup() {
+
+            brackets = new TreesAndBrackets.TreesAndBrackets();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            string t1 = "(())";
+            string t2 = "()";
+
+            Assert.AreEqual("Possible", brackets.check(t1, t2));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            string t1 = "()";
+            string t2 = "()";
+
+            Assert.AreEqual("Possible", brackets.check(t1, t2));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            string t1 = "(()()())";
+            string t2 = "((()))";
+
+            Assert.AreEqual("Impossible", brackets.check(t1, t2));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            string t1 = "((())((())())())";
+            string t2 = "(()(())())";
+
+            Assert.AreEqual("Possible", brackets.check(t1, t2));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            string t1 = "((())((())())())";
+            string t2 = "((()()()()()))";
+
+            Assert.AreEqual("Impossible", brackets.check(t1, t2));
+        }
+    }
+}
---
+++ div1/250/TreesAndBrackets/TreesAndBrackets/TreesAndBrackets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreesAndBrackets {
+    public class TreesAndBrackets {
+
+        public string check(string t1, string t2) {
+
+            return "";
+        }
+    }
+}
