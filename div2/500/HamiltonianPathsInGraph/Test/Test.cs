995ca7c7737853a593c8aea6eef8c88f941ce9b7
YI ZHANG <scaccterran@gmail.com>
2018-04-14 23:50:07 -0400

    SRM 733 div2 1000

---
+++ div2/500/HamiltonianPathsInGraph/Test/Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HamiltonianPathsInGraph;
+
+namespace Test {
+    [TestClass]
+    public class Test {
+
+        HamiltonianPathsInGraph.HamiltonianPathsInGraph path;
+
+        [TestInitialize]
+        public void Setup() {
+
+            path = new HamiltonianPathsInGraph.HamiltonianPathsInGraph();
+        }
+
+        [TestMethod]
+        public void Sample1() {
+
+            string[] edges = { ".+", "-." };
+
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, path.findPath(edges));
+        }
+
+        [TestMethod]
+        public void Sample2() {
+
+            string[] edges = { ".++", "-.+", "--." };
+
+            CollectionAssert.AreEqual(new int[] { 0, 1, 2 }, path.findPath(edges));
+        }
+
+        [TestMethod]
+        public void Sample3() {
+
+            string[] edges = { ".--+", "+.+-", "+-.-", "-++." };
+
+            CollectionAssert.AreEqual(new int[] { 3, 1, 2, 0 }, path.findPath(edges));
+        }
+
+        [TestMethod]
+        public void Sample4() {
+
+            string[] edges = { ".+-+", "-.+-", "+-.-", "-++." };
+
+            CollectionAssert.AreEqual(new int[] { 3, 2, 0, 1 }, path.findPath(edges));
+        }
+
+        [TestMethod]
+        public void Sample5() {
+
+            string[] edges = { ".++--", "-.-++", "-+.+-", "+--.+", "+-+-." };
+
+            CollectionAssert.AreEqual(new int[] { 3, 0, 2, 1, 4 }, path.findPath(edges));
+        }
+    }
+}
