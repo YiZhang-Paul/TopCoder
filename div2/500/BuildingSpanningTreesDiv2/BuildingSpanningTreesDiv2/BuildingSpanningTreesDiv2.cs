ca45c453d271056259cca3cc19e9c6108e59da1b
YI ZHANG <scaccterran@gmail.com>
2018-04-15 09:14:24 -0400

    SRM 733 div2 500

---
+++ div2/500/BuildingSpanningTreesDiv2/BuildingSpanningTreesDiv2/BuildingSpanningTreesDiv2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingSpanningTreesDiv2 {
+    public class BuildingSpanningTreesDiv2 {
+
+        public int getNumberOfSpanningTrees(int n, int[] x, int[] y) {
+
+            return -1;
+        }
+    }
+}
